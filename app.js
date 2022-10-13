1cb7902662f7173c2eea1946b5556123ac4dce35
BRiGHTeSS <93519002+BRiGHTeSS@users.noreply.github.com>
2022-10-13 16:15:56 +0700

    LINK (Click to show PRODUCT DETAIL) MOVE some folder

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const chalk = require('chalk');             // change color in command line
 const debug = require('debug')('app');      // show what's actually working 
 const morgan = require('morgan');           
 const path = require('path');               // path (locate the folders or files)
-const products = require("./data/products.json");       // data products path (locate the folders or files of products)
-const productRouter = express.Router();
 
 const app = express();                      
 const PORT = process.env.PORT || 4000;                          // port (localhost)
+const productRouter = require("./scr/router/productRouter");
 
 
 app.use(morgan('combined'));
@@ -16,18 +15,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req,res) =>{
-    res.render("products",
-        products,
-    );
-});
-
-productRouter.route("/:id").get((req,res) =>{
-    const id = req.params.id;
-    res.send("Hello World !! I'm Product "+id);
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req,res) =>{
 

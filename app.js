b5fe796f4781092c33eb24d550cf59bf934dbb87
BRiGHTeSS <93519002+BRiGHTeSS@users.noreply.github.com>
2022-10-13 12:35:12 +0700

    Bootstrap Template

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const app = express();
-const port = 3000;
+const chalk = require('chalk');             // change color in command line
+const debug = require('debug')('app');      // show what's actually working 
+const morgan = require('morgan');           
+const app = express();                      
+const port = 3000;                          // port (localhost)
+const path = require('path');               // path (locate the folders or files)
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.get("/", (req,res) =>{
 
